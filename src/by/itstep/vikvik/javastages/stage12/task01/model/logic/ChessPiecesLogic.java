6561c3ebc3cb21aa72ba3cd52051b1385b4cbb3f
Victor <ivanvikvik@gmail.com>
2022-04-04 21:54:23 +0300

    Add ChessPiecesLogic class part of implementation

--- src/by/itstep/vikvik/javastages/stage12/task01/model/logic/ChessPiecesLogic.java
+++ src/by/itstep/vikvik/javastages/stage12/task01/model/logic/ChessPiecesLogic.java
@@ -2,6 +2,6 @@ package by.itstep.vikvik.javastages.stage12.task01.model.logic;
 
 public class ChessPiecesLogic {
     public static boolean checkRookStep(int x1, int y1, int x2, int y2) {
-        return false;
+        return (x1 == x2 && y1 != y2) || (y1 == y2 && x1 != x2);
     }
 }
